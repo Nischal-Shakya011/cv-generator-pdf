2054267d4d03655797f3cd3b4a6ca1b15ec5eb62
Nischal Shakya <shakyanischal1111@gmail.com>
2024-08-01 18:00:50 +0545

    validation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,72 @@
-import React from "react";
+import React from 'react';
+import { useFormik } from 'formik';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { formSchema } from '@/validationSchema/formSchema';
+import { Box, Grid, Typography } from '@material-ui/core';
 
-export default function Home() {
+
+const CvForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+// console.log(formik);
   return (
-   <div>
-    
-   </div>
-  
+    <div>
+
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '8px',
+        bgcolor: 'red',
+        color: 'white', 
+      }}
+      >
+        <Typography variant="h5">
+        Fill up the form
+        </Typography>
+      </Box>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Grid container justifyContent='center' alignItems='center'>
+        <Button color="primary" variant="contained" type="submit">
+          Submit
+        </Button>
+        </Grid>
+      </form>
+    </div>
   );
-}
+};
+
+export default CvForm;
