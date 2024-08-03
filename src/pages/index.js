b02fe1fd02155a38e23944246779ad0be1dbd414
Nischal Shakya <shakyanischal1111@gmail.com>
2024-08-03 23:28:01 +0545

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,82 @@
 import React from 'react';
+import { useState } from 'react';
 import { useFormik } from 'formik';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import { formSchema } from '@/validationSchema/formSchema';
-import { Box, Grid, Typography } from '@material-ui/core';
-
+import { Box, Grid, Typography } from '@mui/material';
 
 const CvForm = () => {
+  const [formValue, setFormValue] = useState()
+  console.log(formValue.fullName);
+  
   const formik = useFormik({
     initialValues: {
       email: '',
-      password: '',
+      fullName: '', // Ensure all form fields have initial values
     },
     validationSchema: formSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+          // console.log(values);
+          setFormValue(values)
+      // alert(JSON.stringify(values, null, 2));
     },
   });
-// console.log(formik);
-  return (
-    <div>
 
+
+  return (
+    <div className='p-6 m-auto'>
       <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '8px',
-        bgcolor: 'red',
-        color: 'white', 
-      }}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '8px',
+          bgcolor: 'black',
+          color: 'white', 
+        }}
       >
         <Typography variant="h5">
-        Fill up the form
+          Fill up the form
         </Typography>
       </Box>
       <form onSubmit={formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        <Grid container justifyContent='center' alignItems='center'>
-        <Button color="primary" variant="contained" type="submit">
-          Submit
-        </Button>
+        <Grid container spacing={2} py={3}>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              name="email"
+              label="Email"
+              variant="outlined"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              id="fullName"fullName
+              name=""
+              label="Full Name"
+              type="text"
+              variant="outlined"
+              value={formik.values.fullName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.fullName && Boolean(formik.errors.fullName)}
+              helperText={formik.touched.fullName && formik.errors.fullName}
+            />
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="center" alignItems="center" sx={{ marginTop: 2 }}>
+          <Button color="primary" variant="contained" type="submit">
+            Submit
+          </Button>
         </Grid>
       </form>
     </div>
--- src/validationSchema/formSchema.jsx
+++ src/validationSchema/formSchema.jsx
@@ -5,8 +5,13 @@ export const formSchema = yup.object({
       .string('Enter your email')
       .email('Enter a valid email')
       .required('Email is required'),
-    password: yup
-      .string('Enter your password')
-      .min(8, 'Password should be of minimum 8 characters length')
-      .required('Password is required'),
+    // password: yup
+    //   .string('Enter your password')
+    //   .min(8, 'Password should be of minimum 8 characters length')
+    //   .required('Password is required'),
+    fullName: yup.
+    string('Enter your fullname')
+    .matches(/^[A-Za-z]+ [A-Za-z]+$/, 'Enter a valid fullname')
+    .required('Full name is required'),
+
   });
